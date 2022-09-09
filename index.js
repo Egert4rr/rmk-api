d8ecdb820217218c9399e8285bcb5e6b9508a4a6
Opilane TTHK <opilane@tthk.ee>
2022-09-09 15:16:12 +0300

    add /Trails endpoint and update swagger json with relevant data [Delivers#183205259]

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
 
+app.get('/Trails', (req,res) => {
+    res.send(["Elva jõe", "Endla järve"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () =>  {
