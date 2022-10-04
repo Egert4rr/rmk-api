e2d1aa49e64ac5dc0a0f8023d17f88d206addb5c
Opilane TTHK <opilane@tthk.ee>
2022-10-04 14:08:13 +0300

    Add login form to index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const port = process.env.PORT;
 const swaggerUi = require('swagger-ui-express');
@@ -7,12 +8,13 @@ const swaggerDocument = require('./docs/swagger.json');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+
+
 app.use(cors());
 
 require("../rmk-api/SeedDatabaseTrails");
 require("../rmk-api/SeedDatabaseHikers");
 require("../rmk-api/SeedDatabaseHikes");
-
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DBCONNECTIONSTRING);
@@ -24,8 +26,18 @@ require("./Routes/trailRoutes")(app);
 require("./Routes/hikerRoutes")(app);
 require("./Routes/hikeRoutes")(app);
 
+
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+/*From TARpv20, With love <3*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Views/index.html'));
+})
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Views/SignUp.html'));
+})
 
 app.listen(port, () =>  {
     console.log(`API up at: http://localhost:${port}`);
 });
+
