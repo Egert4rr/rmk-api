6eedb7f92bff450617ee864de2cf94c43ad0e1e8
Opilane TTHK <opilane@tthk.ee>
2022-11-17 11:31:03 +0200

    docs init

--- Backend/Routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-const auth = require("../Controllers/authController")
-
-module.exports = (app) => {
-    app.route("/login").post(auth.loginUser)    
-    app.route("/register").post(auth.registerUser)
-}
